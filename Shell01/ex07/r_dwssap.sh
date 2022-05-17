ba943ae34e229b57fc16f6893acef55f349dc067
Minwoo Cho <103352114+minwoo0419@users.noreply.github.com>
2022-05-17 18:58:12 +0900

    Add files via upload

--- Shell01/ex07/r_dwssap.sh
+++ Shell01/ex07/r_dwssap.sh
@@ -1,4 +1,2 @@
-FT_LINE1=
-FT_LINE2=
+cat /etc/passwd | sed '/#/d' | awk "NR%2==0" | cut -d ':' -f1 | rev | sort -r | awk "NR >= $FT_LINE1 && NR <= $FT_LINE2" | tr '\n' ','  | sed 's/,/, /g' | sed 's/, $/ .$/' | tr -d '\n'
 
-cat /etc/passwd | sed '/#/d' | awk "NR%2==0" | cut -d ':' -f1 | rev | sort -r | awk "NR >= $FT_LINE1 && NR <= $FT_LINE2" | tr ' ' ',' | sed 's/,/, /g' | sed '/, $/./' | tr -d '\n'
